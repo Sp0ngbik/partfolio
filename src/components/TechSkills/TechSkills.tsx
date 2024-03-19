9b94b963e4c3f80be993ee0eaf2d470c1dcdd904
Vlad <Sp0ngbik@gmail.com>
2024-03-19 21:46:12 +0300

    ref animation

--- src/components/TechSkills/TechSkills.tsx
+++ src/components/TechSkills/TechSkills.tsx
@@ -1,43 +1,107 @@
-import React, { useEffect } from 'react'
+import React, { RefObject, useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 import s from './techSkills.module.scss'
+import { techArr, toolsArr, uiArr } from '@/common/const/const'
+
+interface AnimationOptions {
+  from: { [key: string]: number | string }
+  to: { [key: string]: number | string }
+}
 
 const TechSkills = () => {
-  const test = ['A', 'B', 'C']
+  const sectionRef: RefObject<HTMLDivElement> = useRef(null)
+
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger)
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.section',
-        markers: true,
-        scrub: 3,
-        start: 'top bottom',
-        end: 'bottom top',
-      },
+
+    const triggerOptions = {
+      trigger: sectionRef.current,
+      markers: true,
+      start: 'center bottom',
+      end: 'bottom top',
+    }
+
+    const tlTech = gsap.timeline({
+      scrollTrigger: triggerOptions,
     })
-    tl.fromTo(
-      '.el',
-      {
-        x: index => (index % 2 ? gsap.utils.random(-100, -300) : gsap.utils.random(100, 300)),
-      },
-      {
-        x: index => (index % 2 ? gsap.utils.random(100, 300) : gsap.utils.random(-100, -300)),
-      }
-    )
+
+    const tlUi = gsap.timeline({
+      scrollTrigger: triggerOptions,
+    })
+    const tlTools = gsap.timeline({
+      scrollTrigger: triggerOptions,
+    })
+
+    const allTechElements = document.querySelectorAll('.tech_skills')
+    const allUiElements = document.querySelectorAll('.ui_skills')
+    const allToolsElements = document.querySelectorAll('.tools')
+
+    const animateElements = (
+      elements: NodeListOf<Element>,
+      animation: GSAPTimeline,
+      options: AnimationOptions
+    ) => {
+      elements.forEach(el => {
+        animation.fromTo(el, { ...options.from }, { ...options.to })
+      })
+    }
+
+    const defaultPosition = {
+      opacity: 1,
+      x: 0,
+      y: 0,
+      scale: 1,
+      duration: 0.5,
+      ease: 'power3.inOut',
+    }
+
+    animateElements(allTechElements, tlTech, {
+      from: { opacity: 0, x: -1000, scale: 0 },
+      to: defaultPosition,
+    })
+    animateElements(allUiElements, tlUi, {
+      from: { opacity: 0, x: 1000, scale: 0 },
+      to: defaultPosition,
+    })
+    animateElements(allToolsElements, tlTools, {
+      from: { opacity: 0, scale: 0, y: -200 },
+      to: defaultPosition,
+    })
   }, [])
 
   return (
-    <section className={'section'}>
+    <section className={'section'} ref={sectionRef}>
       <div className={s.techsSection}>
         <h2>My Techs</h2>
         <div className={s.skills}>
-          {test.map((el, index) => (
-            <div className="el" key={index}>
-              {el}
+          <div className={s.techBlock}>
+            <div>
+              <span>Logic</span>
+              {techArr.map((el, index) => (
+                <div key={index} className={'tech_skills'}>
+                  {el}
+                </div>
+              ))}
             </div>
-          ))}
+            <div>
+              <span>Tools</span>
+              {toolsArr.map((el, index) => (
+                <div key={index} className={'tools'}>
+                  {el}
+                </div>
+              ))}
+            </div>
+            <div>
+              <span>UI Design</span>
+              {uiArr.map((el, index) => (
+                <div key={index} className={'ui_skills'}>
+                  {el}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </section>
