31d52e3fd48ae66b360da952d79ec1a4409f390a
Vlad <Sp0ngbik@gmail.com>
2024-03-29 16:10:56 +0300

    3d refactor

--- src/components/TechSkills/TechSkills.tsx
+++ src/components/TechSkills/TechSkills.tsx
@@ -4,7 +4,6 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 import s from './techSkills.module.scss'
 import { techArr, toolsArr, uiArr } from '@/common/const/const'
-import { ThreeDCardDemo } from '@/components/Card/SuperCard'
 
 interface AnimationOptions {
   from: { [key: string]: number | string }
